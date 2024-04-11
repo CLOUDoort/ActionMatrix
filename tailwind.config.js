ba543662299f788c3a36e9cc8b0eca490c34269b
JunSeok <ppu1234@naver.com>
2024-04-11 23:55:39 +0900

    style: 구글 폰트 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: 'Crimson Pro, sans-serif',
+      paragraph: 'Dosis',
+    },
+    extend: {
+      height: {
+        screen: '100dvh',
+      },
+    },
   },
   plugins: [],
 };
