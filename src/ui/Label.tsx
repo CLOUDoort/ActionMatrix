db7e9f4bb183d3b0b285dbe85972e8d662c6a61c
JunSeok <ppu1234@naver.com>
2024-04-20 23:53:55 +0900

    feat: Label Component

---
+++ src/ui/Label.tsx
@@ -0,0 +1,19 @@
+const Label = ({ type }: { type: string }) => {
+  const base = ['Title', 'Progress', 'Priority'];
+  const items: { [key: string]: string[] } = {
+    default: [...base, ',Difficulty / Subtask'],
+    filter: [...base, ',Difficulty'],
+    detail: base,
+  };
+  return (
+    <div className="flex w-full border-b-2 h-11 whitespace-nowrap lg:text-h5 border-slate-200">
+      {items[type].map((item: string) => (
+        <div key={item} className="w-28 lg:w-52">
+          {item}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Label;
