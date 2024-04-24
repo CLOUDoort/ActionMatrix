c14a429de863efc21d95ffaf941134f87aeb0d44
JunSeok <ppu1234@naver.com>
2024-04-24 23:53:26 +0900

    style: Label Component text size 변경

--- src/ui/Label.tsx
+++ src/ui/Label.tsx
@@ -1,14 +1,14 @@
 const Label = ({ type }: { type: string }) => {
   const base = ['Title', 'Progress', 'Priority'];
   const items: { [key: string]: string[] } = {
-    default: [...base, ',Difficulty / Subtask'],
-    filter: [...base, ',Difficulty'],
+    default: [...base, 'Difficulty / Subtask'],
+    filter: [...base, 'Difficulty'],
     detail: base,
   };
   return (
-    <div className="flex w-full border-b-2 h-11 whitespace-nowrap lg:text-h5 border-slate-200">
+    <div className="flex w-full text-sm border-b-2 h-11 whitespace-nowrap lg:text-h5 border-slate-200">
       {items[type].map((item: string) => (
-        <div key={item} className="w-28 lg:w-52">
+        <div key={item} className="w-24 lg:w-52">
           {item}
         </div>
       ))}
