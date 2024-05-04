cc2f4015033a02bf00ff221d19732e9d2fd825fd
JunSeok <ppu1234@naver.com>
2024-05-04 21:03:41 +0900

    type: Task type 추가

---
+++ src/@types/Task.ts
@@ -0,0 +1,20 @@
+declare module 'Task' {
+  export interface SubtaskItemInterface {
+    id: string;
+    title: string;
+    details: string;
+    difficulty: string;
+  }
+  export interface SubtaskInterface {
+    [key: string]: SubtaskItemInterface[];
+  }
+  export interface TaskInterface {
+    id: string;
+    title: string;
+    details: string;
+    priority: string;
+    progress: number;
+    difficulty?: string;
+    subtask?: SubtaskInterface;
+  }
+}
