fc843840479985c51849e0dea5f3e8f1293685dd
JunSeok <ppu1234@naver.com>
2024-05-05 23:54:56 +0900

    feat: Subtask context API 추가

---
+++ src/features/create/SubtaskContext.tsx
@@ -0,0 +1,102 @@
+import { SubtaskInterface, SubtaskItemInterface } from 'Task';
+import { ReactNode, createContext, useContext, useReducer } from 'react';
+
+interface ValueInterface {
+  subtask: SubtaskInterface;
+  createSubtask: (subtask: SubtaskItemInterface) => void;
+  modifySubtask: (pre: SubtaskItemInterface, cur: SubtaskItemInterface) => void;
+  deleteSubtask: (subtask: SubtaskItemInterface) => void;
+  clearSubtask: () => void;
+}
+
+type ActionInterface = {
+  type: string;
+  payload?: any;
+};
+
+const SubtaskContext = createContext<ValueInterface | null>(null);
+
+const initialState = {
+  hard: [],
+  normal: [],
+  easy: [],
+};
+
+const reducer = (state: SubtaskInterface, action: ActionInterface): SubtaskInterface => {
+  switch (action.type) {
+    case 'subtask/created': {
+      return {
+        ...state,
+        [action.payload.difficulty]: [...state[action.payload.difficulty], action.payload],
+      };
+    }
+    case 'subtask/modified': {
+      return {
+        ...state,
+        [action.payload.prev.difficulty]: state[action.payload.prev.difficulty].filter(
+          (el) => el.id !== action.payload.prev.id,
+        ),
+        [action.payload.cur.difficulty]: [...state[action.payload.cur.difficulty], action.payload.cur],
+      };
+    }
+    case 'subtask/deleted': {
+      return {
+        ...state,
+        [action.payload.difficulty]: state[action.payload.difficulty].filter((el) => el.id !== action.payload.id),
+      };
+    }
+    case 'subtask/clear': {
+      return initialState;
+    }
+    default:
+      throw new Error('Unknown Action type.');
+  }
+};
+
+const SubtaskProvider = ({ children }: { children: ReactNode }) => {
+  const [subtask, dispatch] = useReducer(reducer, initialState);
+
+  const createSubtask = (subtask: SubtaskItemInterface) => {
+    dispatch({
+      type: 'subtask/created',
+      payload: subtask,
+    });
+  };
+
+  const modifySubtask = (prev: SubtaskItemInterface, cur: SubtaskItemInterface) => {
+    dispatch({
+      type: 'subtask/modified',
+      payload: {
+        prev,
+        cur,
+      },
+    });
+  };
+
+  const deleteSubtask = (subtask: SubtaskItemInterface) => {
+    dispatch({
+      type: 'subtask/deleted',
+      payload: subtask,
+    });
+  };
+
+  const clearSubtask = () => {
+    dispatch({
+      type: 'subtask/clear',
+    });
+  };
+
+  return (
+    <SubtaskContext.Provider value={{ subtask, createSubtask, modifySubtask, deleteSubtask, clearSubtask }}>
+      {children}
+    </SubtaskContext.Provider>
+  );
+};
+
+const useSubtask = () => {
+  const context = useContext(SubtaskContext);
+  if (context === null) throw new Error('Subtask Context was used outside the SubtaskProvider');
+  return context;
+};
+
+export { SubtaskProvider, useSubtask };
--- src/ui/AppLayout.tsx
+++ src/ui/AppLayout.tsx
@@ -1,12 +1,15 @@
+import AppNav from './Navigation/AppNav';
 import { Outlet } from 'react-router-dom';
-import AppNav from './AppNav';
+import { SubtaskProvider } from '../features/create/SubtaskContext';
 
 const AppLayout = () => {
   return (
     <div className="flex w-full h-screen">
       <AppNav />
       <main className="flex justify-center flex-1">
-        <Outlet />
+        <SubtaskProvider>
+          <Outlet />
+        </SubtaskProvider>
       </main>
     </div>
   );
