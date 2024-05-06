ffc040e44304e22bcd7c92930ae6761dc1630539
JunSeok <ppu1234@naver.com>
2024-05-06 19:55:29 +0900

    feat: AppMain Component

--- src/ui/AppMain.tsx
+++ src/ui/AppMain.tsx
@@ -1,20 +1,20 @@
 import Button from './Button';
+import { HiOutlinePlus } from 'react-icons/hi2';
 import { ReactNode } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { HiOutlinePlus } from 'react-icons/hi';
 
 const AppMain = ({ children, name }: { children: ReactNode; name: string }) => {
   const navigate = useNavigate();
   return (
-    <div className="grid w-full h-full grid-rows-[auto_1fr] gap-5 px-8 py-10 max-w-7xl lg:px-16">
-      <div className="flex items-center justify-between h-16 pb-5 md:text-h3 text-h4 ">
+    <div className="grid w-full h-full grid-rows-[auto_1fr] gap-3 lg:px-14 px-5 py-10 max-w-7xl">
+      <div className="flex items-center justify-between h-16 pb-5 lg:text-h3 text-h4 ">
         <span>{name}</span>
         <Button type="create" handler={() => navigate('/app/create')}>
           <HiOutlinePlus size={20} />
           <span>New Task</span>
         </Button>
       </div>
-      <div className="flex flex-col 2xl:pb-28">{children}</div>
+      <div className="flex flex-col">{children}</div>
     </div>
   );
 };
