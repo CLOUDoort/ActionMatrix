c2bde782595267b8752606283134511f3c9e6b6e
JunSeok <ppu1234@naver.com>
2024-05-06 19:22:06 +0900

    feat: No task Component

--- src/ui/NoTasks.tsx
+++ src/ui/NoTasks.tsx
@@ -1,20 +1,22 @@
 import Button from './Button';
-import { HiOutlinePlus } from 'react-icons/hi';
-import { VscEmptyWindow } from 'react-icons/vsc';
+import { HiOutlinePlus } from 'react-icons/hi2';
 import { useNavigate } from 'react-router-dom';
+import { HiOutlineFolderPlus } from 'react-icons/hi2';
 
 const NoTasks = () => {
   const navigate = useNavigate();
   return (
-    <div className="flex flex-col items-center justify-center w-56 gap-6">
-      <VscEmptyWindow className="lg:size-20 size-16" />
-      <div className="flex flex-col items-center justify-center gap-3">
-        <h2 className="text-h4 lg:text-h3 text-Button">No Tasks</h2>
-        <p className="mb-5">There is no pending task</p>
-        <Button type="create" handler={() => navigate('/app/create')}>
-          <HiOutlinePlus size={20} />
-          <span>New Task</span>
-        </Button>
+    <div className="flex items-center justify-center w-full h-full p-10">
+      <div className="flex flex-col items-center justify-center w-56 gap-5">
+        <HiOutlineFolderPlus className="lg:size-20 size-16" />
+        <div className="flex flex-col items-center justify-center gap-3">
+          <h2 className="text-h4 lg:text-h3 text-Button">No Tasks</h2>
+          <p className="mb-3">There is no pending task</p>
+          <Button type="create" handler={() => navigate('/app/create')}>
+            <HiOutlinePlus size={20} />
+            <span>New Task</span>
+          </Button>
+        </div>
       </div>
     </div>
   );
