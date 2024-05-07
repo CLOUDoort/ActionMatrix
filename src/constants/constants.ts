312c40bf6cb1daa749e6aecf105aac3b1f39eec8
JunSeok <ppu1234@naver.com>
2024-05-07 13:01:56 +0900

    feat: Constants 추가

---
+++ src/constants/constants.ts
@@ -0,0 +1,11 @@
+export const Difficulty = Object.freeze({
+  hard: 'hard',
+  normal: 'normal',
+  easy: 'easy',
+});
+
+export const Priority = Object.freeze({
+  high: 'high',
+  medium: 'medium',
+  low: 'low',
+});
