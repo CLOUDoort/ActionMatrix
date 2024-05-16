d068ba8de5d4bd45f14f7cdd46f7ada4518214fa
JunSeok <ppu1234@naver.com>
2024-05-16 20:56:50 +0900

    type: type .d.ts 파일로 변경

--- src/@types/Task.d.ts
+++ src/@types/Task.d.ts
@@ -1,15 +1,18 @@
 declare module 'Task' {
   export interface SubtaskItem {
     id: string;
+    taskId: string;
     title: string;
     details: string;
     difficulty: string;
-    progress: number;
     priority: string;
+    complete: boolean;
   }
+
   export interface Subtask {
     [key: string]: SubtaskItem[];
   }
+
   export interface Task {
     id: string;
     title: string;
@@ -18,5 +21,16 @@ declare module 'Task' {
     progress: number;
     difficulty?: string;
     subtask?: Subtask;
+    subtaskNum?: number;
+    completedSubtaskNum?: number;
+  }
+
+  export interface Focus {
+    id: string;
+    taskId: string;
+    title: string;
+    details: string;
+    priority: string;
+    difficulty: string;
   }
 }
