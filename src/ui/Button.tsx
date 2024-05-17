4b0375a6599ab759a7439e357705a58a70609046
JunSeok <ppu1234@naver.com>
2024-05-17 16:32:55 +0900

    feat: Button Component

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -1,35 +1,40 @@
 import { ReactNode } from 'react';
 
-type PropsType = {
-  children: ReactNode;
-  type: string;
-  handler?: any;
-  disabled?: boolean;
-  submit?: boolean;
-  additionalStyle?: string;
-};
-
 const base = 'flex items-center gap-2 px-3 text-base py-2 min-w-[5rem] rounded transition-all';
 const indigoButton = base + ' text-white justify-center bg-Indigo hover:bg-IndigoHover active:bg-IndigoActive';
 
 const styles: { [key: string]: string } = {
   create: indigoButton,
   save: indigoButton,
+  focus: indigoButton,
   subtask:
     base +
     ' w-full text-left text-slate-400 border-dashed border-2 bg-slate-50 hover:bg-slate-100 active:bg-slate-200 transition-all',
   optiontrue: base + ' border-2 justify-center border-black',
-  optionfalse: base + ` border-2 justify-center border-slate-200 text-slate-400`,
-  cancel: base + ' border justify-center border-slate-200',
+  optionfalse: base + ` border-2 justify-center text-slate-400`,
+  cancel:
+    base + ' border justify-center bg-slate-100 border-slate-300 hover:bg-slate-200 active:bg-slate-300 text-slate-500',
+  edit:
+    base +
+    ' w-full border justify-center bg-slate-100 border-slate-300 hover:bg-slate-200 active:bg-slate-300 text-slate-600',
 };
 
-const Button = ({ children, type, handler, disabled = false, submit = false, additionalStyle = '' }: PropsType) => {
+type PropsType = {
+  children: ReactNode;
+  type: string;
+  handler?: any;
+  disabled?: boolean;
+  submit?: boolean;
+  conditionStyle?: string;
+};
+
+const Button = ({ children, type, handler, disabled = false, submit = false, conditionStyle = '' }: PropsType) => {
   return (
     <button
       type={submit ? 'submit' : 'button'}
       onClick={handler}
       disabled={disabled}
-      className={`${styles[type]} ${additionalStyle}`}
+      className={`${styles[type]} ${conditionStyle}`}
     >
       {children}
     </button>
