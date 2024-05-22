8e0ee00fba31d579815e88bc252cc4d2c2f13240
JunSeok <ppu1234@naver.com>
2024-05-22 18:32:37 +0900

    chore: add vercel analytics & speed-insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,21 @@
 import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
 
+import { Analytics } from '@vercel/analytics/react';
 import App from './App.tsx';
+import { CreateSubtaskContextProvider } from './features/create/CreateSubtaskContext.tsx';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { ToastContainer } from 'react-toastify';
-import { CreateSubtaskContextProvider } from './features/create/CreateSubtaskContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <CreateSubtaskContextProvider>
       <App />
     </CreateSubtaskContextProvider>
+    <Analytics />
+    <SpeedInsights />
     <ToastContainer />
   </React.StrictMode>,
 );
