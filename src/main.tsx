7b3711f2c771bb6511ffb7256c84909539ba2a4f
JunSeok <ppu1234@naver.com>
2024-05-31 23:41:23 +0900

    feat: Add queryClient provider & react query devtools

--- src/main.tsx
+++ src/main.tsx
@@ -4,18 +4,24 @@ import 'react-toastify/dist/ReactToastify.css';
 import { Analytics } from '@vercel/analytics/react';
 import App from './App.tsx';
 import { CreateSubtaskContextProvider } from './features/create/CreateSubtaskContext.tsx';
+import { QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { SpeedInsights } from '@vercel/speed-insights/react';
 import { ToastContainer } from 'react-toastify';
+import { queryClient } from './services/queryClient.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <CreateSubtaskContextProvider>
-      <App />
-    </CreateSubtaskContextProvider>
-    <Analytics />
-    <SpeedInsights />
-    <ToastContainer />
+    <QueryClientProvider client={queryClient}>
+      <CreateSubtaskContextProvider>
+        <App />
+      </CreateSubtaskContextProvider>
+      <ReactQueryDevtools />
+      <Analytics />
+      <SpeedInsights />
+      <ToastContainer />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
