ca09bfcb6d1d00035ef07f423c7f7ab33a8d4de3
JunSeok <ppu1234@naver.com>
2024-06-07 11:51:45 +0900

    feat: Loader Component

---
+++ src/ui/Loader.tsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className="absolute inset-0 flex items-center justify-center">
+      <div className="w-12 h-12 border-4 border-Indigo border-b-transparent rounded-[50%] animate-spin"></div>
+    </div>
+  );
+};
+
+export default Loader;
