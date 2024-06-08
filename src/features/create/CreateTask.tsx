5ccda851c087db0cc60b808fba79534e430d973a
JunSeok <ppu1234@naver.com>
2024-06-08 19:59:06 +0900

    feat: CreateSubtaskContext Provider 적용

--- src/features/create/CreateTask.tsx
+++ src/features/create/CreateTask.tsx
@@ -1,13 +1,16 @@
+import { CreateSubtaskContextProvider } from './CreateSubtaskContext';
 import CreateTaskForm from './form/CreateTaskForm';
 
 const CreateTask = () => {
   return (
-    <section className="flex flex-col w-full max-h-[50rem] gap-3 px-5 py-9 lg:px-14 max-w-7xl">
-      <div className="flex items-center justify-between h-16 pb-5 lg:text-h3 text-h4">
-        <p>New Task</p>
-      </div>
-      <CreateTaskForm />
-    </section>
+    <CreateSubtaskContextProvider>
+      <section className="flex flex-col w-full max-h-[50rem] gap-3 px-5 py-9 lg:px-14 max-w-7xl">
+        <div className="flex items-center justify-between h-16 pb-5 lg:text-h3 text-h4">
+          <p>New Task</p>
+        </div>
+        <CreateTaskForm />
+      </section>
+    </CreateSubtaskContextProvider>
   );
 };
 
