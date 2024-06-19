45cd236582dab52e9d8e0fc01cbbdd4b4bb0a2d3
JunSeok <ppu1234@naver.com>
2024-06-19 13:10:06 +0900

    style: update loader style

--- src/ui/Loader.tsx
+++ src/ui/Loader.tsx
@@ -1,6 +1,6 @@
 const Loader = () => {
   return (
-    <div className="absolute inset-0 flex items-center justify-center">
+    <div className="flex items-center justify-center w-full h-full">
       <div className="w-12 h-12 border-4 border-Indigo border-b-transparent rounded-[50%] animate-spin"></div>
     </div>
   );
