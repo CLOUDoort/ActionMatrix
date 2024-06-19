10a0e277e39f04a5fcb6a288597f196cac90b268
JunSeok <ppu1234@naver.com>
2024-06-19 13:15:03 +0900

    feat: user version API

--- src/services/apiUserTasks.ts
+++ src/services/apiUserTasks.ts
@@ -2,22 +2,18 @@ import type { Focus, SubtaskItem, Task } from 'Task';
 
 import supabase from './supabase';
 import { formattedDate } from '@/utils/formattedDate';
-import { PostgrestError } from '@supabase/supabase-js';
-
-const handleError = (error: PostgrestError | null, message: string): void => {
-  if (error) throw new Error(`${message}: ${error.message}`);
-};
+import { handleError } from '@/ui/HandleError';
 
 export const getUserTask = async (type: string, taskId: string): Promise<Task> => {
   const { data: task, error } = await supabase.from(type).select('*').eq('id', taskId).single();
-  handleError(error, 'Error getting task:');
+  handleError(error, 'Error getting task: ');
 
   return task;
 };
 
 export const getUserTasks = async (type: string, priority: string | null, difficulty: string | null) => {
   const userId = localStorage.getItem('userId')!;
-  let query = supabase.from(type).select('*').eq('user_id', userId);
+  let query = supabase.from(type).select('*').eq('userId', userId);
 
   if (priority) query = query.eq('priority', priority);
 
@@ -25,16 +21,16 @@ export const getUserTasks = async (type: string, priority: string | null, diffic
 
   // 쿼리 실행 및 에러 처리
   const { data: tasks, error } = await query;
-  handleError(error, 'Error getting tasks');
+  handleError(error, 'Error getting tasks: ');
 
   return tasks;
 };
 
 export const createUserTask = async (type: string, task: Task) => {
   task.updatedAt = formattedDate(new Date());
-  const { error } = await supabase.from(type).insert([task]);
+  const { error } = await supabase.from(type).upsert(task).select();
 
-  handleError(error, 'Error creating task');
+  handleError(error, 'Error creating task: ');
 };
 
 export const updateUserTask = async (origin: string, task: Task) => {
@@ -45,20 +41,18 @@ export const updateUserTask = async (origin: string, task: Task) => {
 
   // 기존의 task가 origin 테이블과 동일할 경우 해당 테이블에서 task를 업데이트
   if (origin === targetTable) {
-    const { error } = await supabase.from(targetTable).update(task).eq('id', task.id);
-    handleError(error, 'Error updating task');
+    const { error } = await supabase.from(targetTable).upsert(task).select();
+    handleError(error, 'Error updating task: ');
   } else {
     // 기존의 task가 origin 테이블과 다를 경우 origin 테이블에서 task 삭제하고 target 테이블에 추가
     await deleteUserTask(oppositeTable, task.id);
     await createUserTask(targetTable, task);
   }
-
-  return targetTable;
 };
 
 export const deleteUserTask = async (type: string, taskId: string) => {
   const { error } = await supabase.from(type).delete().eq('id', taskId);
-  handleError(error, 'Error deleting task');
+  handleError(error, 'Error deleting task: ');
 };
 
 export const finishUserTask = async (focus: Focus) => {
@@ -75,23 +69,16 @@ export const finishUserSubtask = async (focus: Focus) => {
     throw new Error('Error: no subtask!');
   }
   const subtasks = task.subtask[focus.difficulty];
-
   const subtaskIndex = subtasks.findIndex((el: SubtaskItem) => el.id === focus.id);
   if (subtaskIndex !== -1) subtasks[subtaskIndex].complete = true;
 
   task.completedSubtaskNum++;
 
-  let flag = false;
   // 모든 subtask 완료
   if (task.subtaskNum === task.completedSubtaskNum) {
-    task.progress = 100;
-    await createUserTask('done', task);
     await deleteUserTask('todo', task.id);
-    flag = true;
+    await createUserTask('done', { ...task, progress: 100 });
   } else {
-    task.progress = Math.trunc((task.completedSubtaskNum / task.subtaskNum) * 100);
-    await updateUserTask('todo', task);
+    await updateUserTask('todo', { ...task, progress: Math.trunc((task.completedSubtaskNum / task.subtaskNum) * 100) });
   }
-
-  return flag;
 };
