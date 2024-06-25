4a2f08357c277355b3e0a98cb12df791637c22e0
JunSeok <ppu1234@naver.com>
2024-06-25 16:46:07 +0900

    fix: create or update task 시 priority or difficulty nav click 금지

--- src/ui/Navigation/AppNavLink.tsx
+++ src/ui/Navigation/AppNavLink.tsx
@@ -1,4 +1,4 @@
-import { NavLink, useSearchParams } from 'react-router-dom';
+import { NavLink, useLocation, useSearchParams } from 'react-router-dom';
 
 import { ucFirst } from '../../utils/ucFirst';
 import { activeLink, normalLink } from './AppNavList';
@@ -9,13 +9,24 @@ type PropsType = {
   value: string;
 };
 
+const checkUrl = (url: string) => {
+  const pattern = /create|update/;
+  return pattern.test(url);
+};
+
 const AppNavLink = ({ to, section, value }: PropsType) => {
   const [searchParams] = useSearchParams();
+  const location = useLocation();
+  const isForm = checkUrl(location.pathname);
   const isActiveParams = searchParams.get(section) === value;
   const name = ucFirst(value);
 
   return (
-    <NavLink to={to} end className={isActiveParams ? activeLink : normalLink}>
+    <NavLink
+      to={to}
+      end
+      className={`${isForm && 'pointer-events-none opacity-50'} ${isActiveParams ? activeLink : normalLink}`}
+    >
       <img src={`/${name}.svg`} alt={value} className="size-6" />
       <span>{name}</span>
     </NavLink>
